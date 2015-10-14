89e953a401adbb4ae1351eb9f659bcbee3a1a4a1
cjds <cjds@live.com>
2015-10-13 21:33:47 -0400

    updates to algorithm

--- Parser.py
+++ Parser.py
@@ -1,10 +1,14 @@
 import re
+import numpy as np
+import math
 
 class Parser(object):
 	"""
+		v1.0
 		This defines a generalized parse dispatcher; 
 		All parse functions reside in subclasses.
 
+		v1.01
 		NOTE: New optimization. Since MS Kinect creates a new face for each vertex,
 		and does not reuse vertices, we can avoid reading the part of the file that 
 		contains information about the faces
@@ -12,17 +16,33 @@ class Parser(object):
 		Time after  optimization: 3min approx
 
 		File size decrease by deleting normals 
+
+		v1.02 Still Ongoing
+		-------------------
+		Attempting to do a heirarchial cluster on the faces that we want.
+		Assuming that a face is a combination of an objects v and vn.
+		Only running on faces that are close to normal
+
+
+		TODO:
+		- If someone can figure out how to run the file reading from the vn 
+		  instead of v, we can move much faster by not bothering to parse extra v
+		- I've noticed that every 3 normals, have similar values. Maybe, we can skip
+		  extra work by only reading one and skipping the other two if it is negative
+		- Replace negative y look with the following code because it is a better test 
+			for an angle math.degrees(self.angle_with_y(self.vn[1]))
+
 	"""
 
 	def __init__(self, file_name):
 		self.file_name = file_name
-		self.v = {}
+		self.v = {} 
 		self.vn = {}
-		self.f = {}
+		self.f={}
 		self.VNIndex=0
 		self.read_file(file_name)
 
-		# return (self.v, self.vn, self.f)
+		# return (self,.v, self.vn, self.f)
 
 	def read_file(self, file_name):
 	   	objFile = open(file_name, 'r')
@@ -31,6 +51,9 @@ class Parser(object):
 	   	for line in objFile:
 		   	if not self.parse(line):
 		   		break;
+
+		#reparse to group into faces
+		#find_centers(self.f,2)
 
 	def write_file(self,output_file_name):
 		outputFile = open(output_file_name, 'w')
@@ -42,10 +65,25 @@ class Parser(object):
 			outStr = "vn " + " ".join(str(x) for x in self.vn[key]) + "\n"
 			outputFile.write(outStr)
 
-		vnkeys=self.vn.keys()
-		for k in range(len(vnkeys)/3):
-			outStr = "f " + str(vnkeys[k]) + "//" +  str(vnkeys[k]) + " " + str(vnkeys[k+1]) + "//" +  str(vnkeys[k+1]) + " " + str(vnkeys[k+2]) + "//" +  str(vnkeys[k+2]) + "\n";
+		#print all the numbers from 1 onwards
+		for k in range(len(self.vn.keys())/3):
+			outStr = "f " + str(3*k+1)+ "//" +  str(3*k+1) + " " + str(3*k+2) + "//" +  str(3*k+2) + " " + str(3*k+3) + "//" +  str(3*k+3) + "\n";
 			outputFile.write(outStr)
+
+	def generate_faces(self):
+		#make faces available to memory
+		vnkeys= self.vn.keys()
+		for k in range(len(vnkeys)/3):
+			vnkey=vnkeys[k]
+			self.f[vnkey]={}
+			self.f[vnkey].v=[]
+			self.f[vnkey].v.append(self.v[k])
+			self.f[vnkey].v.append(self.v[k+1])
+			self.f[vnkey].v.append(self.v[k+2])
+			self.f[vnkey].vn=[]
+			self.f[vnkey].vn.append(self.vn[k])
+			self.f[vnkey].vn.append(self.vn[k+1])
+			self.f[vnkey].vn.append(self.vn[k+2])
 
 	def parse(self, line):
 	   	"""Determine what type of line we are and dispatch appropriately."""
@@ -60,7 +98,6 @@ class Parser(object):
 			self.parse_vn(line)
 
 		elif line[0:2] == "f ":
-			#self.parse_f(line)
 			return False
 
 		return True
@@ -73,6 +110,7 @@ class Parser(object):
 
 	def parse_vn(self, line):
 		xyz = tuple(map(float, line.split()[1:]))
+		#Using self.VNIndex so that we can delete the extra normals 
 		self.VNIndex=self.VNIndex+1
 		#Only keep faces where the y-vector normals >= 0
 		if (xyz[1] >= 0):
@@ -80,26 +118,42 @@ class Parser(object):
 		else:
 			self.v.pop(self.VNIndex)
 
-	def parse_f(self, line):
-		"""
-			Only keep faces where the y-vector normals >= 0
-			Reduced number of faces from 613498 to 379666
-		"""
+	def angle_with_y(self,v1):
+		"""Dot product, then normalize, then offset from origin"""
+		v2=[0,1,0]
+		cosang = np.dot(v1, v2)
+		sinang = np.linalg.norm(np.cross(v1, v2))
+		return np.arctan2(sinang, cosang)
 
-		currentInd = len(self.f)+1
-
-		# (x, xn, y, yn, z, zn) <-- format
-		checkFace = map(int, re.split(' |//',line)[1:])
-
-		# Check values for y normals to ensure all are positive
-		# otherwise delete those extra normals and vertices that are present
-		ynormals = [self.vn[checkFace[1]][1], self.vn[checkFace[3]][1], self.vn[checkFace[5]][1]]
-		if all(vny >= 0 for vny in ynormals):
-			self.f[currentInd] = tuple(checkFace)
-		else:
-			del self.vn[checkFace[1]]
-			del self.vn[checkFace[3]]
-			del self.vn[checkFace[5]]
-			del self.v[checkFace[0]]
-			del self.v[checkFace[2]]
-			del self.v[checkFace[4]]
+	def cluster_points(X, mu):
+		clusters  = {}
+		for x in X:
+        	bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) \
+					for i in enumerate(mu)], key=lambda t:t[1])[0]
+		try:
+			clusters[bestmukey].append(x)
+		except KeyError:
+			clusters[bestmukey] = [x]
+		return clusters
+ 
+	def reevaluate_centers(mu, clusters):
+    	newmu = []
+    	keys = sorted(clusters.keys())
+		for k in keys:
+        	newmu.append(np.mean(clusters[k], axis = 0))
+    	return newmu
+ 
+	def has_converged(mu, oldmu):
+    	return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu])
+ 
+	def find_centers(X, K):
+    	# Initialize to K random centers
+    	oldmu = random.sample(X, K)
+    	mu = random.sample(X, K)
+    	while not has_converged(mu, oldmu):
+        	oldmu = mu
+        	# Assign all points in X to clusters
+        	clusters = cluster_points(X, mu)
+        	# Reevaluate centers
+        	mu = reevaluate_centers(oldmu, clusters)
+    	return(mu, clusters)
--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 from Parser import Parser
 
 
-file_name = 'Chair.obj'
-output_file_name = "FinalChair2.obj.txt"
+file_name = 'sphere.obj'
+output_file_name = "sphere.obj.txt"
 
 
 """ 
@@ -15,21 +15,4 @@ output_file_name = "FinalChair2.obj.txt"
 #outputFile = open(output_file_name, 'w')
 parsedObj = Parser(file_name)
 # print parsedObj.v[1]
-parsedObj.write_file(output_file_name)
-'''
-for key,value in parsedObj.v.items():
-	outStr = "v " + str(value[0]) + " " +  str(value[1]) + " " +  str(value[2]) + "\n"
-	outputFile.write(outStr)
-
-for key,value in parsedObj.vn.items():
-	outStr = "vn " + str(value[0]) + " " +  str(value[1]) + " " +  str(value[2]) + "\n"
-	outputFile.write(outStr)
-
-for key,value in parsedObj.f.items():
-	outStr = "f " + str(value[0]) + "//" +  str(value[1]) + " " + str(value[2]) + "//" +  str(value[3]) + " " + str(value[4]) + "//" +  str(value[5]) + "\n";
-	outputFile.write(outStr)
-'''
-# for ifv in range(1,len(parsedObj.f)):
-# 	outStr = "f " + str(parsedObj.f[ivn][0]) + "//" +  str(parsedObj.f[ivn][1]) + " " + str(parsedObj.f[ivn][2]) + "//" +  str(parsedObj.f[ivn][3]) + " " + str(parsedObj.f[ivn][4]) + "//" +  str(parsedObj.f[ivn][5]) + "\n";
-# 	outputFile.write(outStr)
-
+#parsedObj.write_file(output_file_name)
